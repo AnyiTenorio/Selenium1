93e163163b53d074776378dcca731b4b02b2c513
Anyi Tenorio <anyitenorio3@gmail.com>
2025-10-31 21:12:59 -0600

    Mi primer request Selenium

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,16 +1,35 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+
 namespace Selenium1
 {
     public class Tests
     {
+        //Declaración de la variable a utilizar
+        private IWebDriver driver;
+
         [SetUp]
         public void Setup()
         {
+            //Levantar un navegador limpio, nada de nuestros registros
+            var options = new EdgeOptions();
+            //options.AddArguments("--start-maximized");
+            driver = new EdgeDriver(options);
+            driver.Manage().Window.Maximize();
         }
 
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            //Navega en la ruta indicada
+            driver.Navigate().GoToUrl("https://www.bing.com");
+        }
+
+        [TearDown]
+        public void finalizar()
+        {
+            //Cierra el navegador
+            driver.Quit();
         }
     }
 }
